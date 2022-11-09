f6e57d4bf7676a0dc33c7fd6b6f64827d59123ef
momostafa18 <mohamed.abdelrahman01@eng-st.cu.edu.eg>
2022-11-09 14:39:38 +0200

    First Commit

---
+++ Debug/temp_sensor_example.d
@@ -0,0 +1,10 @@
+temp_sensor_example.o temp_sensor_example.o: ../temp_sensor_example.c \
+  ../adc.h ../std_types.h ../lcd.h ../lm35_sensor.h
+
+../adc.h:
+
+../std_types.h:
+
+../lcd.h:
+
+../lm35_sensor.h:
